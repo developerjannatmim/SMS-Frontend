eacd1933eb1dd492325b6180f5b89dac7664323f
developerjannatmim <mim266758@gmail.com>
2023-10-09 18:03:24 +0600

    fixed admin edit info

--- src/components/forms/AdminForm/AdminForm.js
+++ src/components/forms/AdminForm/AdminForm.js
@@ -1,108 +1,118 @@
 import { Button, Grid } from '@mui/material';
 import { Formik } from 'formik';
+import Select from '@mui/material/Select';
+import * as React from 'react';
 import * as Yup from 'yup';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
 
 import getAdminInitialValues from './getAdminInitialValues';
 import InputField from '../../InputField';
 
 const adminValidationSchema = Yup.object().shape({
-    name: Yup.string().required(),
-    email: Yup.string().email().required(),
-    password: Yup.string().min(6).required(),
-    gender: Yup.string().required(),
-    birthday: Yup.string().required(),
-    address: Yup.string().required(),
-    phone: Yup.string().required(),
-    photo: Yup.string().required(),
-    blood_group: Yup.string().required()
+  name: Yup.string().required(),
+  email: Yup.string().email().required(),
+  password: Yup.string().min(6).required(),
+  gender: Yup.string().required(),
+  birthday: Yup.string().required(),
+  address: Yup.string().required(),
+  phone: Yup.string().required(),
+  photo: Yup.string().required(),
+  blood_group: Yup.string().required()
 });
+const ITEM_HEIGHT = 28;
+const ITEM_PADDING_TOP = 4;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
 
 const AdminForm = ({ admin, onSubmit }) => {
-    return (admin === undefined || admin !== null) && (
-        <Formik
-            initialValues={getAdminInitialValues(admin)}
-            onSubmit={onSubmit}
-            validationSchema={adminValidationSchema}
-        >
-        {({
-            handleSubmit,
-            isSubmitting,
-        }) => (
-            <from noValidate onSubmit={handleSubmit}>
+  let userInformation;
+  try {
+    userInformation = JSON.parse(admin.user_information);
+  } catch (error) { /**/ }
+  
+  return (
+    (admin === undefined || admin !== null) && (
+      <Formik initialValues={getAdminInitialValues(admin)} onSubmit={onSubmit} validationSchema={adminValidationSchema}>
+        {({ handleSubmit, isSubmitting }) => (
+          <from noValidate onSubmit={handleSubmit}>
             <Grid container spacing={3}>
-                <InputField 
-                    label="Name"
-                    id="name"
-                    name="name"
-                    placeholder="Enter name"
-                />
-                <InputField 
-                    label="Email"
-                    id="email"
-                    name="email"
-                    placeholder="Enter email"
-                    type="email"
-                />
-                <InputField 
-                    label="Password"
-                    id="password"
-                    name="password"
-                    placeholder="Enter password"
-                    type="password"
-                />
-                <InputField 
-                    label="Gender"
-                    id="gender"
-                    name="gender"
-                    placeholder="Enter gender"
-                />
-                <InputField 
-                    label="Birthday"
-                    id="birthday"
-                    name="birthday"
-                    placeholder="Enter birthday"
-                />
-                <InputField 
-                    label="Address"
-                    id="address"
-                    name="address"
-                    placeholder="Enter address"
-                />
-                <InputField 
-                    label="Phone"
-                    id="phone"
-                    name="phone"
-                    placeholder="Enter phone"
-                />
-                <InputField 
-                    label="Photo"
-                    id="photo"
-                    name="photo"
-                    placeholder="Enter photo"
-                />
-                <InputField 
-                    label="Blood Group"
-                    id="blood_group"
-                    name="blood_group"
-                    placeholder="Enter blood group"
-                />
-
-                <Grid item xs={12}>
-                    <Button
-                    color="primary"
-                    enabled={isSubmitting}
-                    fullWidth
-                    type="submit"
-                    variant="contained"
-                    >
-                      Submit
-                    </Button>
-                </Grid>
+              <InputField
+                label="Name"
+                id="name"
+                name="name"
+                placeholder="Enter name" />
+              <InputField
+                label="Email"
+                id="email"
+                name="email"
+                placeholder="Enter email"
+                type="email" />
+              <InputField
+                label="Password"
+                id="password"
+                name="password"
+                placeholder="Enter password" type="password" />
+              <InputField
+                label="Gender"
+                id="gender"
+                name="gender"
+                placeholder="Enter gender" />
+              <InputField
+                label="Birthday"
+                id="birthday"
+                name="birthday"
+                placeholder="Enter birthday" />
+              <InputField
+                label="Address"
+                id="address"
+                name="address"
+                placeholder="Enter address" />
+              <InputField
+                label="Phone"
+                id="phone"
+                name="phone"
+                placeholder="Enter phone" />
+              <InputField
+                label="Photo"
+                id="photo"
+                name="photo"
+                placeholder="Enter photo" />
+              <FormControl sx={{ m: 3, width: 485 }}>
+                <InputLabel id="multiple-name-label">Blood Group</InputLabel>
+                <Select
+                  labelId="multiple-name-label"
+                  id="multiple-name"
+                  value={userInformation?.blood_group}
+                  MenuProps={MenuProps}
+                >
+                  <option value={userInformation?.blood_group == 'a+' ? 'a+': ''}>A+</option>
+                  <option value={userInformation?.blood_group == 'a' ? 'a': ''}>A</option>
+                  <option value={userInformation?.blood_group == 'a-' ? 'a-': ''}>A-</option>
+                  <option value={userInformation?.blood_group == 'ab+' ? 'ab+': ''}>AB+</option>
+                  <option value={userInformation?.blood_group == 'ab-' ? 'ab-': ''}>AB-</option>
+                  <option value={userInformation?.blood_group == 'o' ? 'o': ''}>O</option>
+                </Select>
+              </FormControl>
+              <InputField label="Blood Group" id="blood_group" name="blood_group" placeholder="Enter blood group" />
+              <Grid item xs={12}>
+                <Button color="primary" disabled={isSubmitting} fullWidth type="submit" variant="contained">
+                  Submit
+                </Button>
+              </Grid>
             </Grid>
           </from>
         )}
-        </Formik>
+      </Formik>
     )
-}
+  );
+};
 
 export default AdminForm;
--- src/components/forms/AdminForm/getAdminInitialValues.js
+++ src/components/forms/AdminForm/getAdminInitialValues.js
@@ -2,11 +2,11 @@ const getAdminInitialValues = admin => ({
     name: admin?.name || '',
     email: admin?.email || '',
     password: admin?.password || '',
-    gender: admin?.gender || '',
-    birthday: admin?.class_id || '',
-    address: admin?.address || '',
-    phone: admin?.phone || '',
-    photo: admin?.photo || '',
-    blood_group: admin?.blood_group || ''
+    gender: JSON.parse(admin.user_information)?.gender || '',
+    birthday: JSON.parse(admin.user_information)?.birthday || '',
+    address: JSON.parse(admin.user_information)?.address || '',
+    phone: JSON.parse(admin.user_information)?.phone || '',
+    photo: JSON.parse(admin.user_information)?.photo || '',
+    blood_group: JSON.parse(admin.user_information)?.blood_group || ''
 });
 export default getAdminInitialValues;
